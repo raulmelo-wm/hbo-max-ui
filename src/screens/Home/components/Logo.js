f04b0da7a06fce899285d845a373bc96b7769228
Raul Melo <raul.de.melo@warnermedia.com>
2020-11-12 14:10:44 +0100

    clean up

--- src/screens/Home/components/Logo.js
+++ src/screens/Home/components/Logo.js
@@ -1,5 +1,4 @@
 import { Lightning, Utils } from "@lightningjs/sdk";
-import { Color } from "../../../utils/Color";
 
 export class Logo extends Lightning.Component {
   static _template() {
@@ -7,7 +6,6 @@ export class Logo extends Lightning.Component {
       w: 187,
       h: 32,
       y: 57,
-      color: Color.hexToArgb("#ffffff"),
       x: (1920 - 187) / 2,
       src: Utils.asset("images/logo.png"),
     };
