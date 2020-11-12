d8fb8e099cf2a9d1cea7e8307f4f2c397a039bdd
Raul Melo <raul.de.melo@warnermedia.com>
2020-11-12 14:23:04 +0100

    render svg instead png

--- src/screens/Home/components/Logo.js
+++ src/screens/Home/components/Logo.js
@@ -3,11 +3,13 @@ import { Lightning, Utils } from "@lightningjs/sdk";
 export class Logo extends Lightning.Component {
   static _template() {
     return {
-      w: 187,
-      h: 32,
       y: 57,
       x: (1920 - 187) / 2,
-      src: Utils.asset("images/logo.png"),
+      texture: Lightning.Tools.getSvgTexture(
+        Utils.asset("images/logo.svg"),
+        187,
+        32
+      ),
     };
   }
 }
